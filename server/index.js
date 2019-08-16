19f197a1d690a20bfa0a2fde4935097136526076
AlexDoss42 <alex.doss42@gmail.com>
2019-08-15 19:39:56 -0600

    didn't work the first time

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ require('dotenv').config({ path: 'variables.env' });
       console.log(message);
     });
 
-    app.set('port', process.env.PORT || 5000);
+    app.set('port', process.env.PORT || 4242);
     const server = app.listen(app.get('port'), () => {
       console.log(`Express running → PORT ${server.address().port}`);
     });
