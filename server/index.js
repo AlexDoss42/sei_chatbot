4f1610692e5f1446ed929c65b4417e4608c3ad28
AlexDoss42 <alex.doss42@gmail.com>
2019-08-16 15:58:34 -0600

    Adds pusher code to server

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ require('dotenv').config({ path: 'variables.env' });
     const express = require('express');
     const bodyParser = require('body-parser');
     const cors = require('cors');
+    var Pusher = require('pusher');
 
     const app = express();
 
@@ -10,6 +11,17 @@ require('dotenv').config({ path: 'variables.env' });
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({ extended: true }));
 
+    var channels_client = new Pusher({
+      appId: '843700',
+      key: 'd149510c78a9199279a5',
+      secret: '7b7f39303a0551681578',
+      cluster: 'us3',
+      encrypted: true
+    });
+
+    channels_client.trigger('my-channel', 'my-event', {
+      "message": "hello world"
+    });
 
     app.post('/chat', (req, res) => {
       const { message } = req.body;
