2ee35f4a7abee79aa762b41cb62e8e567b82fdf2
AlexDoss42 <alex.doss42@gmail.com>
2019-08-17 22:31:36 -0600

    Still a bunch of errors but got alert

--- server.js
+++ server.js
@@ -4,8 +4,9 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 var processMessage = require('./process-message');
+var Pusher = require('pusher');
 
-const { PORT } = process.env
+const { PUSHER_APP_ID, PUSHER_APP_KEY, PUSHER_APP_SECRET, PUSHER_APP_CLUSTER, PORT } = process.env
 
 const app = express();
 
@@ -13,7 +14,20 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.post('/chat', (req, res) => {
+
+var channels_client = new Pusher({
+  appId: PUSHER_APP_ID,
+  key: PUSHER_APP_KEY,
+  secret: PUSHER_APP_SECRET,
+  cluster: PUSHER_APP_CLUSTER,
+  encrypted: true
+});
+
+channels_client.trigger('my-channel', 'my-event', {
+  "message": "hello world"
+});
+
+app.post('http://localhost:4242/chat', (req, res) => {
   const { message } = req.body;
   console.log(message)
   processMessage(message);
